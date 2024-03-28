90aa543a52f51082f48aa439e0675b775d76b264
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-03-29 00:06:37 +0800

    Proper quotation stuff

--- app/controllers/quotations_controller.rb
+++ app/controllers/quotations_controller.rb
@@ -1,30 +1,29 @@
 class QuotationsController < ApplicationController
-  before_action :set_quotation, only: %i[ show edit update destroy ]
+  before_action :set_quotation, only: %i[show edit update destroy]
 
   # GET /quotations
   def index
-    @quotations = Quotation.all
+    @quotations = Quotation.all.order(created_at: :desc)
   end
 
   # GET /quotations/1
-  def show
-  end
+  def show; end
 
   # GET /quotations/new
   def new
-    @quotation = Quotation.new
+    @quotation = Quotation.new(issue_at: Date.today, due_at: Date.today + 7.days)
+    @quotation.generate_number
   end
 
   # GET /quotations/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /quotations
   def create
     @quotation = Quotation.new(quotation_params)
 
     if @quotation.save
-      redirect_to @quotation, notice: "Quotation was successfully created."
+      redirect_to @quotation, notice: 'Quotation was successfully created.'
     else
       render :new, status: :unprocessable_entity
     end
@@ -33,7 +32,7 @@ class QuotationsController < ApplicationController
   # PATCH/PUT /quotations/1
   def update
     if @quotation.update(quotation_params)
-      redirect_to @quotation, notice: "Quotation was successfully updated.", status: :see_other
+      redirect_to @quotation, notice: 'Quotation was successfully updated.', status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
@@ -42,17 +41,19 @@ class QuotationsController < ApplicationController
   # DELETE /quotations/1
   def destroy
     @quotation.destroy!
-    redirect_to quotations_url, notice: "Quotation was successfully destroyed.", status: :see_other
+    redirect_to quotations_url, notice: 'Quotation was successfully destroyed.', status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_quotation
-      @quotation = Quotation.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def quotation_params
-      params.require(:quotation).permit(:customer_id, :number, :status, :issue_at, :due_at)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_quotation
+    @quotation = Quotation.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def quotation_params
+    params.require(:quotation).permit(:customer_id, :number, :status, :issue_at, :due_at,
+                                      line_attributes: %i[id lineable_type lineable_id description quantity unit_price total_price _destroy])
+  end
 end
--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -1,4 +1,14 @@
 class Quotation < ApplicationRecord
   belongs_to :customer
   has_many :lines, as: :lineable
+  accepts_nested_attributes_for :lines, allow_destroy: true
+
+  enum :status, %w[draft sent rejected accepted], default: :draft
+
+  validates :customer, presence: true
+  validates :due_at, comparison: { greater_than: :issue_at }
+
+  def generate_number
+    self.number = "Q#{Time.now.strftime('%Y%m%d')}#{rand(1000..9999)}"
+  end
 end
