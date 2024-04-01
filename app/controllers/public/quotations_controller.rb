e300b43f705c5d9ef79cb91e35d91cefd229daba
Kennedy Wee <kennedyweesupitang@gmail.com>
2024-04-01 17:53:43 +0800

    Using namespace for public, admin and portal

---
+++ app/controllers/public/quotations_controller.rb
@@ -0,0 +1,59 @@
+class Public::QuotationsController < PublicController
+  before_action :set_quotation, only: %i[show edit update destroy]
+
+  # GET /quotations
+  def index
+    @quotations = Quotation.all.order(created_at: :desc)
+  end
+
+  # GET /quotations/1
+  def show; end
+
+  # GET /quotations/new
+  def new
+    @quotation = Quotation.new(issue_at: Date.today, due_at: Date.today + 7.days)
+    @quotation.generate_number
+  end
+
+  # GET /quotations/1/edit
+  def edit; end
+
+  # POST /quotations
+  def create
+    @quotation = Quotation.new(quotation_params)
+
+    if @quotation.save
+      redirect_to @quotation, notice: 'Quotation was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /quotations/1
+  def update
+    if @quotation.update(quotation_params)
+      redirect_to @quotation, notice: 'Quotation was successfully updated.', status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /quotations/1
+  def destroy
+    @quotation.destroy!
+    redirect_to quotations_url, notice: 'Quotation was successfully destroyed.', status: :see_other
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_quotation
+    @quotation = Quotation.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def quotation_params
+    params.require(:quotation).permit(:customer_id, :number, :status, :issue_at, :due_at,
+                                      lines_attributes: %i[id lineable_type lineable_id description quantity unit_price total_price _destroy position])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,17 @@ Rails.application.routes.draw do
                sign_up: 'register'
              }
 
+  namespace :portal do
+    root 'quotations#index'
+    resources :quotations
+  end
+
+  namespace :public do
+  end
+
+  namespace :admin do
+    resources :quotations
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
